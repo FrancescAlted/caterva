aac2fc36c4994a31dce20a3ffcc8fad90ed2a3f3
Francesc Alted <faltet@gmail.com>
2019-04-16 19:25:11 +0200

    Add copyrights in source headers

--- tests/main.c
+++ tests/main.c
@@ -1,6 +1,11 @@
 /*
- * Copyright (C) 2018  Francesc Alted
- * Copyright (C) 2018  Aleix Alcacer
+ * Copyright (c) 2018-present, Francesc Alted, Aleix Alcacer.
+ * All rights reserved.
+ *
+ * This source code is licensed under both the BSD-style license (found in the
+ * LICENSE file in the root directory of this source tree) and the GPLv2 (found
+ * in the COPYING file in the root directory of this source tree).
+ * You may select, at your option, one of the above-listed licenses.
  */
 
 #include <stdio.h>
