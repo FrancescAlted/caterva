c8acdad97007583c78ed176c155ded1c413aabae
Aleix Alcacer Sales <aleixalcacer@gmail.com>
2020-06-08 00:50:50 +0200

    WIP

---
+++ cunit-tests/main.c
@@ -0,0 +1,10 @@
+//
+// Created by Aleix Alcacer Sales on 08/06/2020.
+//
+
+#include <caterva.h>
+int main() {
+    caterva_config_t cfg = CATERVA_CONFIG_DEFAULTS;
+    cfg.complevel = 10;
+    return 0;
+}
