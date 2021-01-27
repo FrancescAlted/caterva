db74d3739b8fa0bfc794579c15d13162aabe8d4b
Aleix Alcacer Sales <aleixalcacer@gmail.com>
2021-01-27 09:31:08 +0100

    Improve copy performance (#57)
    * Improve copy performance
    
    * Fix cpplint error
    
    * Fix error parsing copy
    
    * Resolve conflicts and update blosc

--- caterva/caterva_blosc.c
+++ caterva/caterva_blosc.c
@@ -1054,14 +1054,76 @@ int caterva_blosc_array_copy(caterva_context_t *ctx, caterva_params_t *params,
                              caterva_storage_t *storage, caterva_array_t *src,
                              caterva_array_t **dest) {
     CATERVA_UNUSED_PARAM(params);
-    int64_t start[CATERVA_MAX_DIM] = {0, 0, 0, 0, 0, 0, 0, 0};
 
-    int64_t stop[CATERVA_MAX_DIM];
+    bool equals = true;
+    if (src->storage == CATERVA_STORAGE_PLAINBUFFER) {
+        equals = false;
+    } else {
+        if (ctx->cfg->compcodec != src->sc->compcode || ctx->cfg->complevel != src->sc->clevel) {
+            equals = false;
+        }
+        for (int i = 0; i < BLOSC2_MAX_FILTERS; ++i) {
+            if (ctx->cfg->filters[i] != src->sc->filters[i]) {
+                equals = false;
+                break;
+            }
+            if (ctx->cfg->filtersmeta[i] != src->sc->filters_meta[i]) {
+                equals = false;
+                break;
+            }
+        }
+    }
     for (int i = 0; i < src->ndim; ++i) {
-        stop[i] = src->shape[i];
+        if (src->chunkshape[i] != storage->properties.blosc.chunkshape[i]) {
+            equals = false;
+            break;
+        }
+        if (src->blockshape[i] != storage->properties.blosc.blockshape[i]) {
+            equals = false;
+            break;
+        }
     }
 
-    CATERVA_ERROR(caterva_array_get_slice(ctx, src, start, stop, storage, dest));
+    if (equals) {
+        CATERVA_ERROR(caterva_array_empty(ctx, params, storage, dest));
+
+        for (int nmetalayer = 0; nmetalayer < src->sc->nmetalayers; ++nmetalayer) {
+            blosc2_metalayer *metalayer = src->sc->metalayers[nmetalayer];
+            if (blosc2_has_metalayer((*dest)->sc, metalayer->name) < 0) {
+                if (blosc2_add_metalayer((*dest)->sc, metalayer->name, metalayer->content,
+                                     metalayer->content_len) < 0) {
+                    CATERVA_ERROR(CATERVA_ERR_BLOSC_FAILED);
+                }
+            }
+        }
+
+        for (int nchunk = 0; nchunk < src->nchunks; ++nchunk) {
+            uint8_t *chunk;
+            bool needs_free;
+            if (blosc2_schunk_get_chunk(src->sc, nchunk, &chunk, &needs_free) < 0) {
+                CATERVA_ERROR(CATERVA_ERR_BLOSC_FAILED);
+            }
+            if (blosc2_schunk_append_chunk((*dest)->sc, chunk, !needs_free) < 0) {
+                CATERVA_ERROR(CATERVA_ERR_BLOSC_FAILED);
+            }
+        }
+
+        if (blosc2_update_usermeta((*dest)->sc, src->sc->usermeta, src->sc->usermeta_len,
+                               *src->sc->storage->cparams) < 0) {
+            CATERVA_ERROR(CATERVA_ERR_BLOSC_FAILED);
+        }
+
+        src->empty = false;
+        src->filled = true;
+    } else {
+        int64_t start[CATERVA_MAX_DIM] = {0, 0, 0, 0, 0, 0, 0, 0};
+
+        int64_t stop[CATERVA_MAX_DIM];
+        for (int i = 0; i < src->ndim; ++i) {
+            stop[i] = src->shape[i];
+        }
+        CATERVA_ERROR(caterva_array_get_slice(ctx, src, start, stop, storage, dest));
+    }
 
     return CATERVA_SUCCEED;
 }
