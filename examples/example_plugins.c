937dd19997594a2fd0d6b2f4d1e55a6d3aaa219d
oscargm98 <soscargm98@gmail.com>
2021-11-10 18:52:51 +0100

    caterva plugins example adapted to a most general version than only ndlz

--- examples/example_plugins.c
+++ examples/example_plugins.c
@@ -1,17 +1,24 @@
 /*
-  Copyright (C) 2021  The Blosc Developers <blosc@blosc.org>
-  https://blosc.org
-  License: BSD 3-Clause (see LICENSE.txt)
-
-  Example program demonstrating use of the Blosc filter from C code.
-
-  To compile this program:
-
-  $ gcc urcodecs.c -o urcodecs -lblosc2
-
-  To run:
-
-  $ ./urcodecs
+* Copyright (C) 2018 Francesc Alted, Aleix Alcacer.
+* Copyright (C) 2019-present Blosc Development team <blosc@blosc.org>
+* All rights reserved.
+*
+* This source code is licensed under both the BSD-style license (found in the
+* LICENSE file in the root directory of this source tree) and the GPLv2 (found
+* in the COPYING file in the root directory of this source tree).
+* You may select, at your option, one of the above-listed licenses.
+*
+* Example program demonstrating use of the Blosc plugins from C code.
+*
+* To compile this program:
+* $ gcc example_plugins.c -o example_plugins -lblosc2
+*
+* To run:
+* $ ./example_plugins
+*
+* from_buffer: 0.2656 s
+* to_buffer: 0.0228 s
+* Process finished with exit code 0
 */
 
 
@@ -44,11 +51,11 @@ int main() {
 
     caterva_config_t cfg = CATERVA_CONFIG_DEFAULTS;
     cfg.nthreads = 1;
-    cfg.splitmode = BLOSC_ALWAYS_SPLIT;
-    cfg.compcodec = BLOSC_CODEC_NDLZ;
-    cfg.compmeta = 4;
+    cfg.compcodec = BLOSC_CODEC_NDLZ;       // use the NDLZ codec trough its plugin
+    cfg.splitmode = BLOSC_ALWAYS_SPLIT;     // needed to work correctly with NDLZ codec
+    cfg.compmeta = 4;                       // needed to work correctly with NDLZ codec
     cfg.complevel = 5;
-
+                                            // we could use a filter plugin by accessing cfg.filters[]
     caterva_ctx_t *ctx;
     caterva_ctx_new(&cfg, &ctx);
 
@@ -77,14 +84,14 @@ int main() {
     blosc_set_timestamp(&t0);
     CATERVA_ERROR(caterva_to_buffer(ctx, arr, buffer, buffer_size));
     blosc_set_timestamp(&t1);
-    printf("get_slice: %.4f s\n", blosc_elapsed_secs(t0, t1));
+    printf("to_buffer: %.4f s\n", blosc_elapsed_secs(t0, t1));
 
     blosc_destroy();
 
     for (int i = 0; i < buffer_size / itemsize; i++) {
         if (src[i] != buffer[i]) {
-            printf("i: %d, data %lld, dest %lld", i, src[i], buffer[i]);
             printf("\n Decompressed data differs from original!\n");
+            printf("i: %d, data %lld, dest %lld", i, src[i], buffer[i]);
             return -1;
         }
     }
