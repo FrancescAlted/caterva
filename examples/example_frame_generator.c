60ce23875d4476db8df0050464d181738e9c10fe
oscargm98 <soscargm98@gmail.com>
2021-12-13 18:39:08 +0100

    some new frames added

--- examples/example_frame_generator.c
+++ examples/example_frame_generator.c
@@ -9,6 +9,7 @@
  */
 
 # include <caterva.h>
+#include <stdlib.h>
 
 int frame_generator(int8_t *data, int8_t ndim, int64_t shape[8], int32_t chunkshape[8],
                     int32_t blockshape[8], int8_t itemsize, int64_t size, char *urlpath) {
@@ -105,8 +106,145 @@ int many_matches() {
     return 0;
 }
 
+int float_cyclic() {
+    int8_t ndim = 3;
+    int64_t shape[] = {40, 60, 20};
+    int32_t chunkshape[] = {20, 30, 16};
+    int32_t blockshape[] = {11, 14, 7};
+    int8_t itemsize = sizeof(float);
+    int64_t nelem = 1;
+    for (int i = 0; i < ndim; ++i) {
+        nelem *= shape[i];
+    }
+    int64_t size = nelem * itemsize;
+
+    float *data = malloc(size);
+    for (int i = 0; i < nelem; i += 2) {
+        float j = (float) i;
+        data[i] = (j + j / 10 + j / 100);
+        data[i + 1] = (2 + j / 10 + j / 1000);
+    }
+    char *urlpath = "example_float_cyclic.caterva";
+    CATERVA_ERROR(frame_generator(data, ndim, shape, chunkshape, blockshape, itemsize, size, urlpath));
+
+    return 0;
+}
+
+int double_same_cells() {
+    int8_t ndim = 2;
+    int64_t shape[] = {40, 60};
+    int32_t chunkshape[] = {20, 30};
+    int32_t blockshape[] = {16, 16};
+    int8_t itemsize = sizeof(double);
+    int64_t nelem = 1;
+    for (int i = 0; i < ndim; ++i) {
+        nelem *= shape[i];
+    }
+    int64_t size = nelem * itemsize;
+
+    double *data = malloc(size);
+    for (int i = 0; i < nelem; i += 4) {
+        data[i] = 1.5;
+        data[i + 1] = 14.7;
+        data[i + 2] = 23.6;
+        data[i + 3] = 3.2;
+    }
+    char *urlpath = "example_double_same_cells.caterva";
+    CATERVA_ERROR(frame_generator(data, ndim, shape, chunkshape, blockshape, itemsize, size, urlpath));
+
+    return 0;
+}
+
+int big_float_frame() {
+    int ndim = 3;
+    int64_t shape[] = {200, 310, 214};
+    int32_t chunkshape[] = {110, 120, 76};
+    int32_t blockshape[] = {57, 52, 35};
+    int8_t itemsize = sizeof(float);
+    int64_t nelem = 1;
+    for (int i = 0; i < ndim; ++i) {
+        nelem *= shape[i];
+    }
+    int64_t size = nelem * itemsize;
+
+    float *data = malloc(size);
+    for (int i = 0; i < nelem; i += 4) {
+        float j = (float) i;
+        data[i] = (float) 2.73;
+        data[i + 1] = (2 + j / 10 + j / 1000);
+        data[i + 2] = (7 + j / 10 - j / 100);
+        data[i + 3] = (11 + j / 100 - j / 1000);
+    }
+    char *urlpath = "example_big_float_frame.caterva";
+    CATERVA_ERROR(frame_generator(data, ndim, shape, chunkshape, blockshape, itemsize, size, urlpath));
+
+    return 0;
+}
+
+int day_month_temp() {
+    int ndim = 2;
+    int64_t shape[] = {400, 3};
+    int32_t chunkshape[] = {110, 3};
+    int32_t blockshape[] = {57, 3};
+    int8_t itemsize = sizeof(float);
+    int64_t nelem = 1;
+    for (int i = 0; i < ndim; ++i) {
+        nelem *= shape[i];
+    }
+    int64_t size = nelem * itemsize;
+
+    float temp_min = -20;
+    float temp_max = 40;
+    srand(time(NULL));
+    float *data = malloc(size);
+    for (int i = 0; i < nelem / 3; i++) {
+        data[i] = (float) (rand() % 31);
+        data[i + 1] = (float) (rand() % 12);
+        data[i + 2] = ((float) (rand() % 10000) / 10000 * (temp_max - temp_min) + temp_min);
+        i += 3;
+    }
+    char *urlpath = "example_day_month_temp.caterva";
+    CATERVA_ERROR(frame_generator(data, ndim, shape, chunkshape, blockshape, itemsize, size, urlpath));
+
+    return 0;
+}
+
+int item_prices() {
+    int ndim = 3;
+    int64_t shape[] = {12, 25, 250};
+    int32_t chunkshape[] = {6, 10, 50};
+    int32_t blockshape[] = {3, 5, 10};
+    int8_t itemsize = sizeof(float);
+    int64_t nelem = 1;
+    for (int i = 0; i < ndim; ++i) {
+        nelem *= shape[i];
+    }
+    int64_t size = nelem * itemsize;
+
+    float price_min = (float) 1;        // if I choose 0.99 results are less aproppiate
+    float price_max = (float) 251;
+    float *data = malloc(size);
+    int index = 0;
+    for (int month = 1; month <= shape[0]; month ++) {               // month (1 to 12)
+        for (int store = 1; store <= shape[1]; store ++) {           // store ID (less to more expensive)
+            for (int item = 1; item <= shape[2]; item++) {           // item ID
+                srand(item);
+                data[index] = ((float) store + (float) (3 - (month % 3)) *
+                               ((float) (rand() % 1000) / 1000 * (price_max - price_min) + price_min));
+                index++;
+            }
+        }
+    }
+    char *urlpath = "example_item_prices.caterva";
+    CATERVA_ERROR(frame_generator(data, ndim, shape, chunkshape, blockshape, itemsize, size, urlpath));
+
+    return 0;
+}
+
+
 int main() {
     int err;
+ /*
     err = all_eq();
     if (err != CATERVA_SUCCEED) {
         printf("\n All_eq error: %d", err);
@@ -118,6 +256,33 @@ int main() {
     err = many_matches();
     if (err != CATERVA_SUCCEED) {
         printf("\n Many_matches error: %d", err);
+    }
+    err = float_cyclic();
+    if (err != CATERVA_SUCCEED) {
+        printf("\n Float_cyclic error: %d", err);
+    }
+    err = double_same_cells();
+    if (err != CATERVA_SUCCEED) {
+        printf("\n Double_same_cells error: %d", err);
+    }
+    err = image1();
+    if (err != CATERVA_SUCCEED) {
+        printf("\n Image1 error: %d", err);
+    }
+
+    err = big_float_frame();
+    if (err != CATERVA_SUCCEED) {
+        printf("\n Double_same_cells error: %d", err);
+    }
+*/
+    err = day_month_temp();
+    if (err != CATERVA_SUCCEED) {
+        printf("\n Day_month_temp error: %d", err);
+    }
+
+    err = item_prices();
+    if (err != CATERVA_SUCCEED) {
+        printf("\n Item_prices error: %d", err);
     }
     return err;
 }
