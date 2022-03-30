540da6aa80cadd3e759ed96a3b6e384ea673a5df
muusbolla <muusbolla@gmail.com>
2022-03-29 19:26:52 -0700

    reduce math in caterva_copy_buffer

--- caterva/caterva_utils.c
+++ caterva/caterva_utils.c
@@ -174,6 +174,157 @@ int32_t deserialize_meta(uint8_t *smeta, uint32_t smeta_len, uint8_t *ndim, int6
     return 0;
 }
 
+// copyNdim where N = {2-8} - specializations of copy loops to be used by caterva_copy_buffer
+// since we don't have c++ templates, substitute manual specializations for up to known CATERVA_MAX_DIM (8)
+// it's not pretty, but it substantially reduces overhead vs. the generic method
+void copy8dim(const uint8_t itemsize, const int64_t* copy_shape,
+uint8_t *bdst, const uint8_t *bsrc, const int64_t* src_strides, const int64_t* dst_strides) {
+    int64_t copy_nbytes = copy_shape[7] * itemsize;
+    int64_t copy_start[7] = {0};
+    while(copy_start[0] < copy_shape[0]) {
+    while(copy_start[1] < copy_shape[1]) {
+    while(copy_start[2] < copy_shape[2]) {
+    while(copy_start[3] < copy_shape[3]) {
+    while(copy_start[4] < copy_shape[4]) {
+    while(copy_start[5] < copy_shape[5]) {
+    while(copy_start[6] < copy_shape[6]) {
+        int64_t src_copy_start = 0;
+        int64_t dst_copy_start = 0;
+        for (int j = 0; j < 7; ++j) {
+            src_copy_start += copy_start[j] * src_strides[j];
+            dst_copy_start += copy_start[j] * dst_strides[j];
+        }
+
+        memcpy(&bdst[dst_copy_start * itemsize], &bsrc[src_copy_start * itemsize], copy_nbytes);
+    ++copy_start[6]; }
+    ++copy_start[5]; copy_start[6] = 0; }
+    ++copy_start[4]; copy_start[5] = 0; copy_start[6] = 0; }
+    ++copy_start[3]; copy_start[4] = 0; copy_start[5] = 0; copy_start[6] = 0; }
+    ++copy_start[2]; copy_start[3] = 0; copy_start[4] = 0; copy_start[5] = 0; copy_start[6] = 0; }
+    ++copy_start[1]; copy_start[2] = 0; copy_start[3] = 0; copy_start[4] = 0; copy_start[5] = 0; copy_start[6] = 0; }
+    ++copy_start[0]; copy_start[1] = 0; copy_start[2] = 0; copy_start[3] = 0; copy_start[4] = 0; copy_start[5] = 0; copy_start[6] = 0; }
+}
+
+void copy7dim(const uint8_t itemsize, const int64_t* copy_shape,
+uint8_t *bdst, const uint8_t *bsrc, const int64_t* src_strides, const int64_t* dst_strides) {
+    int64_t copy_nbytes = copy_shape[6] * itemsize;
+    int64_t copy_start[6] = {0};
+    while(copy_start[0] < copy_shape[0]) {
+    while(copy_start[1] < copy_shape[1]) {
+    while(copy_start[2] < copy_shape[2]) {
+    while(copy_start[3] < copy_shape[3]) {
+    while(copy_start[4] < copy_shape[4]) {
+    while(copy_start[5] < copy_shape[5]) {
+        int64_t src_copy_start = 0;
+        int64_t dst_copy_start = 0;
+        for (int j = 0; j < 6; ++j) {
+            src_copy_start += copy_start[j] * src_strides[j];
+            dst_copy_start += copy_start[j] * dst_strides[j];
+        }
+
+        memcpy(&bdst[dst_copy_start * itemsize], &bsrc[src_copy_start * itemsize], copy_nbytes);
+    ++copy_start[5]; }
+    ++copy_start[4]; copy_start[5] = 0; }
+    ++copy_start[3]; copy_start[4] = 0; copy_start[5] = 0; }
+    ++copy_start[2]; copy_start[3] = 0; copy_start[4] = 0; copy_start[5] = 0; }
+    ++copy_start[1]; copy_start[2] = 0; copy_start[3] = 0; copy_start[4] = 0; copy_start[5] = 0; }
+    ++copy_start[0]; copy_start[1] = 0; copy_start[2] = 0; copy_start[3] = 0; copy_start[4] = 0; copy_start[5] = 0; }
+}
+
+void copy6dim(const uint8_t itemsize, const int64_t* copy_shape,
+uint8_t *bdst, const uint8_t *bsrc, const int64_t* src_strides, const int64_t* dst_strides) {
+    int64_t copy_nbytes = copy_shape[5] * itemsize;
+    int64_t copy_start[5] = {0};
+    while(copy_start[0] < copy_shape[0]) {
+    while(copy_start[1] < copy_shape[1]) {
+    while(copy_start[2] < copy_shape[2]) {
+    while(copy_start[3] < copy_shape[3]) {
+    while(copy_start[4] < copy_shape[4]) {
+        int64_t src_copy_start = 0;
+        int64_t dst_copy_start = 0;
+        for (int j = 0; j < 5; ++j) {
+            src_copy_start += copy_start[j] * src_strides[j];
+            dst_copy_start += copy_start[j] * dst_strides[j];
+        }
+
+        memcpy(&bdst[dst_copy_start * itemsize], &bsrc[src_copy_start * itemsize], copy_nbytes);
+    ++copy_start[4]; }
+    ++copy_start[3]; copy_start[4] = 0; }
+    ++copy_start[2]; copy_start[3] = 0; copy_start[4] = 0; }
+    ++copy_start[1]; copy_start[2] = 0; copy_start[3] = 0; copy_start[4] = 0; }
+    ++copy_start[0]; copy_start[1] = 0; copy_start[2] = 0; copy_start[3] = 0; copy_start[4] = 0; }
+}
+
+void copy5dim(const uint8_t itemsize, const int64_t* copy_shape,
+uint8_t *bdst, const uint8_t *bsrc, const int64_t* src_strides, const int64_t* dst_strides) {
+    int64_t copy_nbytes = copy_shape[4] * itemsize;
+    int64_t copy_start[4] = {0};
+    while(copy_start[0] < copy_shape[0]) {
+    while(copy_start[1] < copy_shape[1]) {
+    while(copy_start[2] < copy_shape[2]) {
+    while(copy_start[3] < copy_shape[3]) {
+        int64_t src_copy_start = 0;
+        int64_t dst_copy_start = 0;
+        for (int j = 0; j < 4; ++j) {
+            src_copy_start += copy_start[j] * src_strides[j];
+            dst_copy_start += copy_start[j] * dst_strides[j];
+        }
+
+        memcpy(&bdst[dst_copy_start * itemsize], &bsrc[src_copy_start * itemsize], copy_nbytes);
+    ++copy_start[3]; }
+    ++copy_start[2]; copy_start[3] = 0; }
+    ++copy_start[1]; copy_start[2] = 0; copy_start[3] = 0; }
+    ++copy_start[0]; copy_start[1] = 0; copy_start[2] = 0; copy_start[3] = 0; }
+}
+
+void copy4dim(const uint8_t itemsize, const int64_t* copy_shape,
+uint8_t *bdst, const uint8_t *bsrc, const int64_t* src_strides, const int64_t* dst_strides) {
+    int64_t copy_nbytes = copy_shape[3] * itemsize;
+    int64_t copy_start[3] = {0};
+    while(copy_start[0] < copy_shape[0]) {
+    while(copy_start[1] < copy_shape[1]) {
+    while(copy_start[2] < copy_shape[2]) {
+        int64_t src_copy_start = 0;
+        int64_t dst_copy_start = 0;
+        for (int j = 0; j < 3; ++j) {
+            src_copy_start += copy_start[j] * src_strides[j];
+            dst_copy_start += copy_start[j] * dst_strides[j];
+        }
+
+        memcpy(&bdst[dst_copy_start * itemsize], &bsrc[src_copy_start * itemsize], copy_nbytes);
+    ++copy_start[2]; }
+    ++copy_start[1]; copy_start[2] = 0; }
+    ++copy_start[0]; copy_start[1] = 0; copy_start[2] = 0; }
+}
+
+void copy3dim(const uint8_t itemsize, const int64_t* copy_shape,
+uint8_t *bdst, const uint8_t *bsrc, const int64_t* src_strides, const int64_t* dst_strides) {
+    int64_t copy_nbytes = copy_shape[2] * itemsize;
+    int64_t copy_start[2] = {0};
+    while(copy_start[0] < copy_shape[0]) {
+    while(copy_start[1] < copy_shape[1]) {
+        int64_t src_copy_start = 0;
+        int64_t dst_copy_start = 0;
+        for (int j = 0; j < 2; ++j) {
+            src_copy_start += copy_start[j] * src_strides[j];
+            dst_copy_start += copy_start[j] * dst_strides[j];
+        }
+
+        memcpy(&bdst[dst_copy_start * itemsize], &bsrc[src_copy_start * itemsize], copy_nbytes);
+    ++copy_start[1]; }
+    ++copy_start[0]; copy_start[1] = 0; }
+}
+
+void copy2dim(const uint8_t itemsize, const int64_t* copy_shape,
+uint8_t *bdst, const uint8_t *bsrc, const int64_t* src_strides, const int64_t* dst_strides) {
+    int64_t copy_nbytes = copy_shape[1] * itemsize;
+    int64_t copy_start = 0;
+    while(copy_start < copy_shape[0]) {
+        int64_t src_copy_start = copy_start * src_strides[0];
+        int64_t dst_copy_start = copy_start * dst_strides[0];
+        memcpy(&bdst[dst_copy_start * itemsize], &bsrc[src_copy_start * itemsize], copy_nbytes);
+    ++copy_start; }
+}
 
 int caterva_copy_buffer(uint8_t ndim,
                         uint8_t itemsize,
@@ -210,30 +361,34 @@ int caterva_copy_buffer(uint8_t ndim,
     for (int i = 0; i < ndim; ++i) {
         copy_shape[i] = src_stop[i] - src_start[i];
     }
-    int64_t copy_nbytes = copy_shape[ndim - 1];
-
-    // Copy contiguous memory blocks
-    int64_t number_of_copies = 1;
-    for (int i = 0; i < ndim - 1; ++i) {
-        number_of_copies *= src_stop[i] - src_start[i];
-    }
-    for (int ncopy = 0; ncopy < number_of_copies; ++ncopy) {
-        // Compute the start of the copy
-        int64_t copy_start[CATERVA_MAX_DIM] = {0};
-        index_unidim_to_multidim(ndim - 1, copy_shape, ncopy, copy_start);
-
-        // Translate this index to the src buffer
-        int64_t src_copy_start;
-        index_multidim_to_unidim(copy_start, ndim - 1, src_strides, &src_copy_start);
-
-        // Translate this index to the dst buffer
-        int64_t dst_copy_start;
-        index_multidim_to_unidim(copy_start, ndim - 1, dst_strides, &dst_copy_start);
-
-        // Perform the copy
-        memcpy(&bdst[dst_copy_start * itemsize],
-               &bsrc[src_copy_start * itemsize],
-               copy_nbytes * itemsize);
+switch(ndim) {
+    case 1:
+        memcpy(&bdst[0], &bsrc[0], copy_shape[0] * itemsize);
+    break;
+    case 2:
+        copy2dim(itemsize, copy_shape, bdst, bsrc, src_strides, dst_strides);
+    break;
+    case 3:
+        copy3dim(itemsize, copy_shape, bdst, bsrc, src_strides, dst_strides);
+    break;
+    case 4:
+        copy4dim(itemsize, copy_shape, bdst, bsrc, src_strides, dst_strides);
+    break;
+    case 5:
+        copy5dim(itemsize, copy_shape, bdst, bsrc, src_strides, dst_strides);
+    break;
+    case 6:
+        copy6dim(itemsize, copy_shape, bdst, bsrc, src_strides, dst_strides);
+    break;
+    case 7:
+        copy7dim(itemsize, copy_shape, bdst, bsrc, src_strides, dst_strides);
+    break;
+    case 8:
+        copy8dim(itemsize, copy_shape, bdst, bsrc, src_strides, dst_strides);
+    break;
+    default:
+        return CATERVA_ERR_INVALID_INDEX; // guard against potential future increase to CATERVA_MAX_DIM
+    break;
     }
 
     return CATERVA_SUCCEED;
